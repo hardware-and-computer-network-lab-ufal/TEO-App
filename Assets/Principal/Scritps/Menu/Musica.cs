3e7cb7b1f3a8adb796dc46cd1905838ad6ff2cf8
Samilly Nunes <samillynunes19@gmail.com>
2019-01-24 15:34:29 -0300

    Menu inicial, telas de saida, nivel, opcoes e informacoes, audioManager

---
+++ Assets/Principal/Scritps/Menu/Musica.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Musica : MonoBehaviour {
+
+	static Musica instance;
+    public AudioSource musicaFundo; 
+
+	void Awake(){
+		if (instance == null) {
+			GameObject.DontDestroyOnLoad (this.gameObject);
+		} else {
+			Destroy (gameObject);
+		}
+
+	}
+
+    private void Update()
+    {
+        if (!musicaFundo.isPlaying)
+        {
+            musicaFundo.Play();
+        }
+    }
+}
+
---
+++ Assets/Principal/Scritps/Niveis/Nivel.cs
@@ -0,0 +1,44 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class Nivel : MonoBehaviour {
+
+    [SerializeField]
+    private GameObject nivelTela;
+    private Animator animacao;
+
+    private void Start()
+    {
+        animacao = nivelTela.GetComponent<Animator>();
+    }
+    public void TelaNivel()
+    {
+        nivelTela.SetActive(true);
+        animacao.Play("tela_nivel");
+        
+    
+    }
+
+    public void Facil()
+    {
+        SceneManager.LoadScene(1);
+    }
+
+    public void Medio()
+    {
+        SceneManager.LoadScene(1);
+    }
+
+    public void Dificil()
+    {
+        SceneManager.LoadScene(1);
+    }
+
+    public void Voltar()
+    {
+        animacao.Play("tela_nivel_inverse");
+    }
+	
+}
