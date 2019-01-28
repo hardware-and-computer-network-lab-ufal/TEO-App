4ff8f518dbc6847cfab4d459c503e9acfe075ace
Samilly Nunes <samillynunes19@gmail.com>
2019-01-28 08:51:33 -0300

    Bug de voltar 'consertado'

--- Assets/Principal/Scritps/Jogos/JogosManager.cs
+++ Assets/Principal/Scritps/Jogos/JogosManager.cs
@@ -2,6 +2,7 @@
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.UI;
+using UnityEngine.SceneManagement;
 
 public class JogosManager : MonoBehaviour {
 
@@ -45,11 +46,26 @@ public class JogosManager : MonoBehaviour {
         foreach (Botao b in lista)
         {
             GameObject botaoObj = Instantiate(botao) as GameObject; //instancia um botao como gameobject da cena
-            botaoObj.transform.SetParent(prateleira, false);
-            Button botaoNovo = botaoObj.GetComponent<Button>();
+            botaoObj.transform.SetParent(prateleira, false); //seta o local para dentro da prateleira(painel)
+            Button botaoNovo = botaoObj.GetComponent<Button>(); //pega o gameobj e transforma num botao
 
-            botaoNovo.image.sprite = Resources.Load<Sprite>("Jogos/" + b.nome);
+            botaoNovo.image.sprite = Resources.Load<Sprite>("Jogos/" + b.nome); //altera a img do botao
+
+            //if (b.nome == "cores") //teste!!!
+            //{
+            //    botaoNovo.onClick.AddListener( //chamando a cena com o nome do jogo
+            //    () => {
+            //        GoJogo(b.nome);
+            //    }
+            //);
+            //}
+            
 
         }
+    }
+
+    void GoJogo(string nome)
+    {
+        SceneManager.LoadScene(nome);
     }
 }
--- Assets/Principal/Scritps/Niveis/NivelManager.cs
+++ Assets/Principal/Scritps/Niveis/NivelManager.cs
@@ -12,6 +12,7 @@ public class NivelManager : MonoBehaviour {
     private Animator animacao;
     public int nivel;
     [SerializeField]private Button playBtn;
+    [SerializeField] private Button facilBtn,medioBtn,dificilBtn; //botoes nivel
 
     private void Awake()
     {
@@ -24,42 +25,77 @@ public class NivelManager : MonoBehaviour {
         {
             Destroy(this.gameObject);
         }
+
+        if (SceneManager.GetActiveScene().buildIndex == 0) //verificando se esta na cena do menu p nao dar erro em outras cenas
+        {
+            SceneManager.sceneLoaded += CarregaCena; //sempre que a cena abrir vai carregar esses objs
+        }
+
+        
     }
 
     private void Start()
     {
         animacao = nivelTela.GetComponent<Animator>();
 
+        SceneManager.sceneLoaded += CarregaCena;
+
+        nivelTela.SetActive(false);
+
+
     }
+
+    void CarregaCena(Scene cena, LoadSceneMode modo) //carregar objetos importantes da cena 
+    {
+        //componentes da tela de níveis
+        nivelTela = GameObject.Find("Niveis");
+        facilBtn = GameObject.Find("inicianteBtn").GetComponent<Button>();
+        medioBtn = GameObject.Find("intermediarioBtn").GetComponent<Button>();
+        dificilBtn = GameObject.Find("avancadoBtn").GetComponent<Button>();
+        facilBtn.onClick.AddListener(() => { Facil(); });
+        medioBtn.onClick.AddListener(() => { Medio(); });
+        dificilBtn.onClick.AddListener(() => { Dificil(); });
+
+        //Botao play
+        playBtn = GameObject.Find("PlayButton").GetComponent<Button>();
+        playBtn.onClick.AddListener(() => { TelaNivel(); });
+    }
+    
     public void TelaNivel()
     {
         nivelTela.SetActive(true);
+        animacao = nivelTela.GetComponent<Animator>();
         animacao.Play("tela_nivel");
-        
-    
+            
     }
 
     public void Facil()
     {
         nivel = 1;
-        print(nivel);
+        Voltar();
+        nivelTela.SetActive(false);
         SceneManager.LoadScene(1);
     }
 
     public void Medio()
     {
         this.nivel = 2;
+        Voltar();
+        nivelTela.SetActive(false);
         SceneManager.LoadScene(1);
     }
 
     public void Dificil()
     {
         this.nivel = 3;
+        Voltar();
+        nivelTela.SetActive(false);
         SceneManager.LoadScene(1);
     }
 
     public void Voltar()
     {
+        animacao = nivelTela.GetComponent<Animator>();
         animacao.Play("tela_nivel_inverse");
     }
 	
