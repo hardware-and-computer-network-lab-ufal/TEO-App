aa44e7c558af6b211782d338a789ae596f4d5f95
Nícholas Lima de Souza Silva <nicholaslima.rw@gmail.com>
2019-02-21 10:18:55 -0300

    Cores Manager

--- Assets/Principal/Scritps/Jogos/Cores/Cores.cs
+++ Assets/Principal/Scritps/Jogos/Cores/Cores.cs
@@ -1,16 +1,76 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.SceneManagement;
 
 public class Cores : MonoBehaviour {
 
-	// Use this for initialization
+	public static Cores instance;
+	public static GameObject panelParabens;
+	public static GameObject panelParabensFinal;
+	private Animator parabensAnim;
+
+	private int contagemCores = 0;
+
+	private void Awake() {
+		if (SceneManager.GetActiveScene().buildIndex == 3) {
+			if ( instance == null)
+				instance = this;
+		}
+	}
+
+	IEnumerator DesligaPanel () {
+		yield return new WaitForSeconds(0.001f);
+		parabensAnim = panelParabens.GetComponent<Animator>();
+		panelParabens.SetActive(false);
+		panelParabensFinal.SetActive(false);
+
+	}
+
+	IEnumerator VoltaPanelParabens() {
+		yield return new WaitForSeconds(5);
+		parabensAnim.Play("painel_parabens_reverse");
+		yield return new WaitForSeconds(1);
+		panelParabensFinal.SetActive(true);
+		panelParabens.SetActive(true);
+	}
+
+	public void JogarNovamente() {
+		SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex);
+
+	}
+
+	public void CoresCompletas(int coresTotais) {
+		if (coresTotais == contagemCores) {
+			panelParabens.SetActive(true);
+			parabensAnim.Play("panel_parabens");
+			StartCoroutine("VoltaPanelParabens");
+		}
+	}
+
+	public void SalvaPontos(int pontosNovos) {
+		if (PlayerPrefs.HasKey("pontos")) {
+			int pontosAtuais = PlayerPrefs.GetInt("pontos");
+			PlayerPrefs.SetInt("pontos", (pontosAtuais + pontosNovos));
+		} else {
+			PlayerPrefs.SetInt("pontos", pontosNovos);
+		}
+	}
+
+	public void TiraPontos(int pontos) {
+		SalvaPontos(-pontos);
+	}
+
+	public int PegarPontos() {
+		return PlayerPrefs.GetInt("pontos");
+	}
 	void Start () {
 		
 	}
 	
 	// Update is called once per frame
 	void Update () {
-		
+		CoresCompletas(6);
 	}
 }
