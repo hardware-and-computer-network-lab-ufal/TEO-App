b20ecc1047b2ea86e842a26b83c6e2920cf25683
Nícholas Lima de Souza Silva <nicholaslima.rw@gmail.com>
2019-03-14 08:48:32 -0300

    Jogo cores for merge (#1)
    * Cores Cena 
    
    Fundo definido
    
    * Cores Manager
    
    * Assets Cores
    
    * Gerando cores aleatoriamente, baseado no nivel
    
    * Objetos aleatórios + posição correta
    
    As cores são geradas randomicamente, armazenadas e posicionadas para caber na tela: acima de 3, as cores são posicionadas acima.
    
    * Destruindo objetos ao contato
    
    * Animação de Parabéns funcionando
    
    * Tempo para destruir o objeto
    
    * Possível animação para a destruição dos objetos
    
    * Limpeza de print em Cores
    
    * Cores acessado pelo menu
    
    Cena incluída no BuildSettings para ser acessada pelo menu
    
    * Resolvendo Conflitos
    
    * Cores acessado pelo menu sem conflito
    
    * Resetando posição da cor ao ser liberada
    
    * BugFix: As cores poderiam diferir, já que é random

---
+++ Assets/Principal/Scritps/Jogos/Cores/CoresDetect.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+// using UnityEngine.UI;
+// using UnityEngine.SceneManagement;
+
+public class CoresDetect : MonoBehaviour {
+
+	
+	void OnCollisionEnter2D (Collision2D obj) {
+		if (obj.gameObject.name == gameObject.name + " (1)") {
+			Destroy(obj.gameObject, 0.2f);
+			Destroy(gameObject, 0.2f);
+			Cores.contagemCores++;
+			// print(Cores.contagemCores);
+		}
+	}
+}
