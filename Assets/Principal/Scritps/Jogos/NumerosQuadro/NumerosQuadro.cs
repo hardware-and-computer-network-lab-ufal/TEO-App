8714ded60b8d6a74bfed92bb06eb4ab170bbd0fc
Nícholas Lima de Souza Silva <nicholaslima.rw@gmail.com>
2019-03-18 09:25:58 -0300

    Arquivos Numeros Quadro

---
+++ Assets/Principal/Scritps/Jogos/NumerosQuadro/NumerosQuadro.cs
@@ -0,0 +1,119 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.SceneManagement;
+
+public class NumerosQuadro : MonoBehaviour {
+
+	public static NumerosQuadro instance;
+	public static GameObject panelParabens;
+	public static GameObject panelParabensFinal;
+	private Animator parabensAnim;
+
+	public static int contagemTotal = 0;
+	// public List<GameObject> circulos;
+	private Queue<int> unicos = new Queue<int>();
+	private int dificuldade;
+
+	private void Awake() {
+		if (SceneManager.GetActiveScene().buildIndex == 5) {
+			if ( instance == null)
+				instance = this;
+		}
+		contagemTotal = 0;
+	}
+
+	IEnumerator DesligaPanel () {
+		yield return new WaitForSeconds(0.001f);
+		parabensAnim = panelParabens.GetComponent<Animator>();
+		panelParabens.SetActive(false);
+		panelParabensFinal.SetActive(false);
+
+	}
+
+	IEnumerator VoltaPanelParabens() {
+		yield return new WaitForSeconds(5);
+		parabensAnim.Play("painel_parabens_reverse");
+		yield return new WaitForSeconds(1);
+		panelParabensFinal.SetActive(true);
+		panelParabens.SetActive(true);
+	}
+
+	public void JogarNovamente() {
+		SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex);
+
+	}
+
+	public void SomaCompleta(int coresTotais) {
+		if (coresTotais == contagemTotal) {
+			// panelParabens.SetActive(true);
+			// parabensAnim.Play("panel_parabens");
+			// StartCoroutine("VoltaPanelParabens");
+			panelParabens.SetActive(true);
+            parabensAnim.Play("panel_parabens");
+            StartCoroutine(VoltaPanelParabens());
+		}
+	}
+	public void sortearCores(){
+		
+		System.Random ordem = new System.Random();
+
+		/* Condicional responsável por sortear as cores que irão aparecer */
+		if (dificuldade == 1) {
+			while (unicos.Count != 2){
+				int numero = ordem.Next(1,7);
+				if(!unicos.Contains(numero))
+					unicos.Enqueue(numero);
+			}
+		} else if (dificuldade == 2){
+			while (unicos.Count != 4){
+				int numero = ordem.Next(1,7);
+				if(!unicos.Contains(numero))
+					unicos.Enqueue(numero);
+			}
+		} else {
+			while (unicos.Count != 6){
+				int numero = ordem.Next(1,7);
+				if(!unicos.Contains(numero))
+					unicos.Enqueue(numero);
+			}
+		}
+	}
+
+	public void SalvaPontos(int pontosNovos) {
+		if (PlayerPrefs.HasKey("pontos")) {
+			int pontosAtuais = PlayerPrefs.GetInt("pontos");
+			PlayerPrefs.SetInt("pontos", (pontosAtuais + pontosNovos));
+		} else {
+			PlayerPrefs.SetInt("pontos", pontosNovos);
+		}
+	}
+
+	public void TiraPontos(int pontos) {
+		SalvaPontos(-pontos);
+	}
+
+	public int PegarPontos() {
+		return PlayerPrefs.GetInt("pontos");
+	}
+	void Start () {
+		panelParabens = GameObject.Find("panel_parabens");
+		panelParabensFinal = GameObject.Find("panel_parabens_final");
+
+		StartCoroutine(DesligaPanel());
+		dificuldade = PlayerPrefs.GetInt("nivel");
+		
+		sortearCores();
+	}
+	
+	void Update () {
+		if (dificuldade == 1)
+			print("D: 1");
+		else if (dificuldade == 2)
+			print("D: 2");
+		else
+			print("D: 3");
+	}
+}
