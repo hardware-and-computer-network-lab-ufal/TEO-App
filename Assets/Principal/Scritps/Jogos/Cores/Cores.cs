8f2b960c96e1a95ec0890750a9c7eea09b07ed72
Nícholas Lima de Souza Silva <nicholaslima.rw@gmail.com>
2019-03-19 08:29:53 -0300

    Develop/feature jogo das cores (#3)
    * Jogo cores for merge (#1)
    
    * Cores Cena 
    
    Fundo definido
    
    * Cores Manager
    
    * Assets Cores
    
    * Gerando cores aleatoriamente, baseado no nivel
    
    * Objetos aleatórios + posição correta
    
    As cores são geradas randomicamente, armazenadas e posicionadas para caber na tela: acima de 3, as cores são posicionadas acima.
    
    * Destruindo objetos ao contato
    
    * Animação de Parabéns funcionando
    
    * Tempo para destruir o objeto
    
    * Possível animação para a destruição dos objetos
    
    * Limpeza de print em Cores
    
    * Cores acessado pelo menu
    
    Cena incluída no BuildSettings para ser acessada pelo menu
    
    * Resolvendo Conflitos
    
    * Cores acessado pelo menu sem conflito
    
    * Resetando posição da cor ao ser liberada
    
    * BugFix: As cores poderiam diferir, já que é random
    
    * Indicador visual de que houve colisão
    
    A cor dos objetos é ligeiramente transformada em cinza.

---
+++ Assets/Principal/Scritps/Jogos/Cores/Cores.cs
@@ -0,0 +1,192 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.SceneManagement;
+
+public class Cores : MonoBehaviour {
+
+	public static Cores instance;
+	public static GameObject panelParabens;
+	public static GameObject panelParabensFinal;
+	private Animator parabensAnim;
+
+	public static int contagemCores = 0;
+	// public List<GameObject> circulos;
+	private Queue<int> unicos = new Queue<int>();
+	private int dificuldade;
+
+	private void Awake() {
+		if (SceneManager.GetActiveScene().buildIndex == 4) {
+			if ( instance == null)
+				instance = this;
+		}
+		contagemCores = 0;
+	}
+
+	IEnumerator DesligaPanel () {
+		yield return new WaitForSeconds(0.001f);
+		parabensAnim = panelParabens.GetComponent<Animator>();
+		panelParabens.SetActive(false);
+		panelParabensFinal.SetActive(false);
+
+	}
+
+	IEnumerator VoltaPanelParabens() {
+		yield return new WaitForSeconds(5);
+		parabensAnim.Play("painel_parabens_reverse");
+		yield return new WaitForSeconds(1);
+		panelParabensFinal.SetActive(true);
+		panelParabens.SetActive(true);
+	}
+
+	public void JogarNovamente() {
+		SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex);
+
+	}
+
+	public void CoresCompletas(int coresTotais) {
+		if (coresTotais == contagemCores) {
+			// panelParabens.SetActive(true);
+			// parabensAnim.Play("panel_parabens");
+			// StartCoroutine("VoltaPanelParabens");
+			panelParabens.SetActive(true);
+            parabensAnim.Play("panel_parabens");
+            StartCoroutine(VoltaPanelParabens());
+		}
+	}
+	public void sortearCores(){
+		
+		System.Random ordem = new System.Random();
+
+		/* Condicional responsável por sortear as cores que irão aparecer */
+		if (dificuldade == 1) {
+			while (unicos.Count != 2){
+				int numero = ordem.Next(1,7);
+				if(!unicos.Contains(numero))
+					unicos.Enqueue(numero);
+			}
+		} else if (dificuldade == 2){
+			while (unicos.Count != 4){
+				int numero = ordem.Next(1,7);
+				if(!unicos.Contains(numero))
+					unicos.Enqueue(numero);
+			}
+		} else {
+			while (unicos.Count != 6){
+				int numero = ordem.Next(1,7);
+				if(!unicos.Contains(numero))
+					unicos.Enqueue(numero);
+			}
+		}
+	}
+	public List<GameObject> GerarCores(string complemento) {
+		List<GameObject> restultado = new List<GameObject>();
+		Queue<int> coresEscolhidas = new Queue<int>();
+		foreach (int item in unicos) {
+			coresEscolhidas.Enqueue(item);
+		}
+
+		
+		// Stack<int> escolha = new Stack<int>();
+
+		/* O complemento é usado para diferenciar as cores que serão arrastadas das fixas, 
+		   e ainda assim mantendo ambas em ordem aleatória.
+		   complemento == " (1)" representa as cores que serão arrastadas, 
+		   já complemento == "" representa as cores que serão fixas. */
+		string[]  coresIndex = new string[7];
+		coresIndex[1] = "COR-AMARELO" + complemento;
+		coresIndex[2] = "COR-AZUL" + complemento;
+		coresIndex[3] = "COR-LARANJA" + complemento;
+		coresIndex[4] = "COR-ROXO" + complemento;
+		coresIndex[5] = "COR-VERDE" + complemento;
+		coresIndex[6] = "COR-VERMELHO" + complemento;
+		
+		/*
+			1- Amarelo
+			2- Azul
+			3- Laranja
+			4- Roxo
+			5- Verde
+			6- Vermelho
+		 */
+
+		while (coresEscolhidas.Count != 0) {
+			int elemento = coresEscolhidas.Dequeue();
+			restultado.Add(GameObject.Find(coresIndex[elemento]));
+		}
+
+		
+		return restultado;
+	}
+	public void CarregarCores() {
+		List<GameObject> circulos = GerarCores("");
+
+		float xStart = -4.7f;
+		float yNovaLinha = 0f;
+		int countCirculos = 0;
+
+		/* Posicionando os círculos aleatórios na tela */
+		foreach (GameObject item in circulos) {
+			if (countCirculos == 3) {
+				yNovaLinha = 2.9f;
+				xStart = -4.7f;
+			}
+			item.transform.position = new Vector3(xStart, (yNovaLinha -2.7f), 0f);
+			xStart +=2.9f;
+			countCirculos++;
+		}
+
+	}
+
+	public void CoresSeleciona() {
+		List<GameObject> escolha = GerarCores(" (1)");
+		// foreach (GameObject item in circulos) {
+		// 	escolha.Add(item);
+		// }
+		float zChange = 95f;
+
+		foreach (GameObject item in escolha) {
+			item.transform.position = new Vector3(5f, -2.68f, zChange);
+			zChange+=1f;
+		}
+	}
+
+	public void SalvaPontos(int pontosNovos) {
+		if (PlayerPrefs.HasKey("pontos")) {
+			int pontosAtuais = PlayerPrefs.GetInt("pontos");
+			PlayerPrefs.SetInt("pontos", (pontosAtuais + pontosNovos));
+		} else {
+			PlayerPrefs.SetInt("pontos", pontosNovos);
+		}
+	}
+
+	public void TiraPontos(int pontos) {
+		SalvaPontos(-pontos);
+	}
+
+	public int PegarPontos() {
+		return PlayerPrefs.GetInt("pontos");
+	}
+	void Start () {
+		panelParabens = GameObject.Find("panel_parabens");
+		panelParabensFinal = GameObject.Find("panel_parabens_final");
+
+		StartCoroutine(DesligaPanel());
+		dificuldade = PlayerPrefs.GetInt("nivel");
+		
+		sortearCores();
+		CarregarCores();
+		CoresSeleciona();
+	}
+	
+	void Update () {
+		if (dificuldade == 1)
+			CoresCompletas(2);
+		else if (dificuldade == 2)
+			CoresCompletas(4);
+		else
+			CoresCompletas(6);
+	}
+}
---
+++ Assets/Principal/Scritps/Jogos/Cores/CoresDetect.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+// using UnityEngine.UI;
+// using UnityEngine.SceneManagement;
+
+public class CoresDetect : MonoBehaviour {
+
+	
+	void OnCollisionEnter2D (Collision2D obj) {
+		if (obj.gameObject.name == gameObject.name + " (1)") {
+			Destroy(obj.gameObject, 0.2f);
+			Destroy(gameObject, 0.2f);
+			Cores.contagemCores++;
+			
+			obj.gameObject.GetComponent<Renderer>().material.SetColor("_Color", Color.gray);
+			gameObject.GetComponent<Renderer>().material.SetColor("_Color", Color.gray);
+		}
+	}
+}
