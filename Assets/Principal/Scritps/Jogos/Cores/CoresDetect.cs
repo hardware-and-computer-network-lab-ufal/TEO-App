f23dea36c0daf31877def76f8a2c5cec689c25d3
Nícholas Lima de Souza Silva <nicholaslima.rw@gmail.com>
2019-04-01 11:48:38 -0300

    Hover sobre cor correta com destruíção ao soltar o mouse

--- Assets/Principal/Scritps/Jogos/Cores/CoresDetect.cs
+++ Assets/Principal/Scritps/Jogos/Cores/CoresDetect.cs
@@ -4,16 +4,35 @@ using UnityEngine;
 
 public class CoresDetect : MonoBehaviour {
 
-	
+	public static CoresDetect instance;
+	private Collision2D atualColisao;
+	private Color atual, fixa;
 	void OnCollisionEnter2D (Collision2D obj) {
-		Cores.instance.destroyCircle = 1;
-		if (obj.gameObject.name == gameObject.name + " (1)" && (Cores.instance.destroyCircle == 2)) {
-			Destroy(obj.gameObject, 0.2f);
-			Destroy(gameObject, 0.2f);
-			Cores.instance.contagemCores++;
-			
+		if (obj.gameObject.name == gameObject.name + " (1)"){
+			Cores.instance.destroyCircle = 1;
+			atual = obj.gameObject.GetComponent<Renderer>().material.GetColor("_Color");
+			fixa = gameObject.GetComponent<Renderer>().material.GetColor("_Color");
 			obj.gameObject.GetComponent<Renderer>().material.SetColor("_Color", Color.gray);
 			gameObject.GetComponent<Renderer>().material.SetColor("_Color", Color.gray);
+			atualColisao = obj;
+		}
+	}
+
+	void OnCollisionExit2D (Collision2D obj) {
+		if (obj.gameObject.name == gameObject.name + " (1)"){
+			Cores.instance.destroyCircle = 0;
+			obj.gameObject.GetComponent<Renderer>().material.color = atual;
+			gameObject.GetComponent<Renderer>().material.color = fixa;
+			atualColisao = null;
+		}
+	}
+
+	void Update(){
+		if (Cores.instance.destroyCircle == 2){
+			Destroy(atualColisao.gameObject, 0.2f);
+			Destroy(gameObject, 0.2f);
+			Cores.instance.contagemCores++;
+			Cores.instance.destroyCircle = 0;
 		}
 	}
 }
