fc2eae74faff87c82ace0c0bdd0e5f89e26a92c1
Nícholas Lima de Souza Silva <nicholaslima.rw@gmail.com>
2019-04-02 11:48:44 -0300

    Efeitos sonoros em QuantoE
    Som de colisão, erro e parabéns

--- Assets/Principal/Scritps/Jogos/NumerosQuadro/NumerosQuadroConferirBtn.cs
+++ Assets/Principal/Scritps/Jogos/NumerosQuadro/NumerosQuadroConferirBtn.cs
@@ -3,15 +3,22 @@ using System.Collections.Generic;
 using UnityEngine;
 
 public class NumerosQuadroConferirBtn : MonoBehaviour {
+	AudioSource som;
+	AudioClip efeito;
 	public void SomaCompleta() {
+		som = NumerosQuadro.instance.GetComponent<AudioSource>();
+		som.Stop();
 		if ((NumerosQuadro.instance.numeroSorteadoUm + NumerosQuadro.instance.numeroSorteadoDois) == NumerosQuadro.instance.somaTotal) {
+			efeito = Resources.Load<AudioClip>("Som/Parabens");
+			som.PlayOneShot(efeito);
 			print("Ganhei, total:");
 			print(NumerosQuadro.instance.somaTotal);
 			NumerosQuadro.instance.panelParabens.SetActive(true);
             NumerosQuadro.instance.parabensAnim.Play("panel_parabens");
             StartCoroutine(NumerosQuadro.instance.VoltaPanelParabens());
 		} else {
-			print("Não foi dessa vez");
+			efeito = Resources.Load<AudioClip>("Som/Errou");
+			som.PlayOneShot(efeito);
 		}
 	}
 }
--- Assets/Principal/Scritps/Jogos/NumerosQuadro/NumerosQuadroDetect.cs
+++ Assets/Principal/Scritps/Jogos/NumerosQuadro/NumerosQuadroDetect.cs
@@ -3,14 +3,23 @@ using System.Collections.Generic;
 using UnityEngine;
 
 public class NumerosQuadroDetect : MonoBehaviour {
-
+	AudioSource som;
+	AudioClip efeito;
 	private void OnCollisionEnter2D (Collision2D obj) {
+		som = NumerosQuadro.instance.GetComponent<AudioSource>();
+		som.Stop();
+		efeito = Resources.Load<AudioClip>("Som/Posicionou");
+		som.PlayOneShot(efeito);
 		NumerosQuadro.instance.somaTotal++;
 		NumerosQuadro.instance.criarFruta();
 		print(NumerosQuadro.instance.somaTotal);
 	}
 
 	private void OnCollisionExit2D (Collision2D obj) {
+		som = NumerosQuadro.instance.GetComponent<AudioSource>();
+		som.Stop();
+		efeito = Resources.Load<AudioClip>("Som/Posicionou");
+		som.PlayOneShot(efeito);
 		NumerosQuadro.instance.somaTotal--;
 		Destroy(obj.gameObject, 0.2f);
 	}
