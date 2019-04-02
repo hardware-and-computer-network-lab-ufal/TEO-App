0dae487f66de2301c50c93c8fafae4b73159a16e
Nícholas Lima de Souza Silva <nicholaslima.rw@gmail.com>
2019-04-02 11:30:36 -0300

    Jogo QuantoE (#2)
    * Arquivos de NumerosQuadro
    
    Jogo para posicionar a quantidade de objetos na tela
    
    * Arquivos Numeros Quadro
    
    * NumerosQuadro Cena
    
    * Trabalhando na exibição dos números de forma economica
    
    * Frutas movidas para resources
    
    * Gerando frutas de forma aleatória
    
    * Adicionando score
    
    Quando a fruta é colocada na caixa-delimitadora, o score é incrementado.
    
    * Desabilitando o movimento da fruta
    
    Ao entrar em contato com a caixa delimitadora, o movimento da fruta é desabilitado para evitar que chegue-se a soma correta com apenas uma única fruta
    
    * NumerosQuadro no fluxo iniciante
    
    * Permitir que a fruta seja removida da caixa + tentativa de clonar fruta
    
    * Frutas sendo duplicadas
    
    Falta tratamento
    
    * Fruta raíz está fora do alcance + nova fruta no momento em que 1 é usada
    
    * Bug: Função de sortear sendo chamada duas vezes #Fixed

---
+++ Assets/Principal/Scritps/Jogos/NumerosQuadro/NumerosQuadroDetect.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class NumerosQuadroDetect : MonoBehaviour {
+
+	private void OnCollisionEnter2D (Collision2D obj) {
+		NumerosQuadro.instance.somaTotal++;
+		NumerosQuadro.instance.criarFruta();
+		print(NumerosQuadro.instance.somaTotal);
+	}
+
+	private void OnCollisionExit2D (Collision2D obj) {
+		NumerosQuadro.instance.somaTotal--;
+		Destroy(obj.gameObject, 0.2f);
+	}
+}
