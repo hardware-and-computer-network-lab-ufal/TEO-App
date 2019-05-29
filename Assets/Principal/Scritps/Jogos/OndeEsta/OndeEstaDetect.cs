dfa79df947612172c411980c2c876425e2730080
Nícholas Lima de Souza Silva <nicholaslima.rw@gmail.com>
2019-05-29 10:45:46 -0300

    Develop/fork for merge (#13)
    * Together into forkForMerge (#8)
    
    * Onde Esta e QuantoE finalizados (#12)
    
    * Merge do OndeEsta (#3)
    
    * Arquivos jogo OndeEsta
    
    * background
    
    * Resources para carregar em tempo de jogo
    
    * Setup inicial QuantoE
    
    * QuantoE Script
    
    * Loading child and question
    
    * OndeEsta for detecting
    
    * Colliders for detecting which part of the body is clicked.
    
    * Playing sound (bugged) and counting right parts
    
    * Audio Fixed + Random body questions
    
    * BuildIndex
    
    * Cleanup
    
    * Scene AVD and OndeEsta enabled
    
    * QuantoE for Merge (#4)
    
    * Jogo QuantoE (#2)
    
    * Arquivos de NumerosQuadro
    
    Jogo para posicionar a quantidade de objetos na tela
    
    * Arquivos Numeros Quadro
    
    * NumerosQuadro Cena
    
    * Trabalhando na exibição dos números de forma economica
    
    * Frutas movidas para resources
    
    * Gerando frutas de forma aleatória
    
    * Adicionando score
    
    Quando a fruta é colocada na caixa-delimitadora, o score é incrementado.
    
    * Desabilitando o movimento da fruta
    
    Ao entrar em contato com a caixa delimitadora, o movimento da fruta é desabilitado para evitar que chegue-se a soma correta com apenas uma única fruta
    
    * NumerosQuadro no fluxo iniciante
    
    * Permitir que a fruta seja removida da caixa + tentativa de clonar fruta
    
    * Frutas sendo duplicadas
    
    Falta tratamento
    
    * Fruta raíz está fora do alcance + nova fruta no momento em que 1 é usada
    
    * Bug: Função de sortear sendo chamada duas vezes #Fixed
    
    * Efeitos sonoros em QuantoE
    
    Som de colisão, erro e parabéns
    
    * BuildIndex NumerosQuadro (QuantoE)
    
    * Both eyes, same name
    
    * Área de detecção da boca aumentada
    
    * Correção na aleatoriedade para englobar todas as partes da face
    
    * OndeEsta nas dificuldades iniciante e intermediário também
    
    * Tela de Estatísticas após tela de parabens/animação de parabéns reverso
    
    * Jogo da Memoria Assets
    
    * Ajustes para gameplay semelhante ao das cores
    
    * Sortear cards
    
    * Objetos na cena de forma aleatória
    
    * Ocultando as cartas após 10 segundos
    
    * Movimento nas cartas
    
    * Selecionando as cartas + contando os acertos
    
    Não é mais arrastando, apenas clicando na carta que queira virar.
    
    * Animação de Parabéns ativada
    
    * Cores pause_popup and sound effects
    
    * Memoria pause_popup and sound effects
    
    * QuantoE pause_popup and sound effects
    
    * OndeEsta pause_popup and sound effects
    
    * OndeEsta instance
    
    * Voltar em Memoria
    
    * Memoria no Fluxo iniciante, intermediário e avançado
    
    * Cards aleatórios
    
    * Memoria no BuildSettings

---
+++ Assets/Principal/Scritps/Jogos/OndeEsta/OndeEstaDetect.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class OndeEstaDetect : MonoBehaviour {
+	bool controller;
+
+	public void detectClicked(string parte) {
+		Vector3 locationMouse;
+		RaycastHit2D hit;
+
+		locationMouse = Camera.main.ScreenToWorldPoint(Input.mousePosition);
+		hit = Physics2D.Raycast(locationMouse, Vector2.zero);
+
+		if (hit.collider != null && hit.transform.gameObject.name == parte) {
+			Musica.instance.OnRight();
+			OndeEsta.instance.contagemOndeEsta++;
+			OndeEsta.instance.changeQuestion = true;
+			
+		} else if (hit.collider != null && hit.transform.gameObject.name != parte) {
+			Musica.instance.OnFail();
+		}
+
+	}
+	void Update() {
+		controller = Input.GetMouseButtonDown(0);
+		if (controller == true) {
+			if (OndeEsta.instance.questao == 0) {
+				detectClicked("olho");
+			} else if (OndeEsta.instance.questao == 1) {
+				detectClicked("nariz");
+			} else if (OndeEsta.instance.questao == 2) {
+				detectClicked("boca");
+			}
+		}
+	}
+}
