0f3b224c8b4911fe52e915f6a09a8741142b28af
Samilly Nunes <samillynunes19@gmail.com>
2019-09-14 16:04:33 -0300

    Tela de informações adicionada

---
+++ Assets/Principal/Scritps/Menu/Sobre.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class Sobre : MonoBehaviour {
+
+    private Button sobreBtn;
+    [SerializeField] private GameObject sobreTela;
+
+	// Use this for initialization
+	void Start () {
+        sobreBtn = gameObject.GetComponent<Button>();
+
+        sobreBtn.onClick.AddListener(
+            () =>
+            {
+                GameObject.Find("Teo-Main").SetActive(false);
+                GameObject.Find("Menu").SetActive(false);
+                sobreTela.SetActive(true);
+                TEOManager.instance.MudaIdioma();
+            }    
+        );
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
