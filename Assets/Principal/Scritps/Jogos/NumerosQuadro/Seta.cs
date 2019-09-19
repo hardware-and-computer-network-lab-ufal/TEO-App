93f697dd22b5633063f34e55e51062a66f3baf49
Nícholas Lima de Souza Silva <nicholaslima.rw@gmail.com>
2019-09-19 08:38:06 -0300

    Revert "Languages" (#17)

---
+++ Assets/Principal/Scritps/Jogos/NumerosQuadro/Seta.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Seta : MonoBehaviour {
+
+    private float tempo = 10.0f;
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        tempo -= Time.deltaTime;
+        print(tempo);
+
+        if (tempo <= 0)
+        {
+            Destroy(this.gameObject);
+        }
+	}
+}
---
+++ Assets/Principal/Scritps/Jogos/SimboNumero/Carteira.cs
@@ -0,0 +1,40 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Carteira : MonoBehaviour {
+    
+
+    private void OnTriggerEnter2D(Collider2D outro) //o objeto (no unity) tem um rigidybody do tipo kinematico pq nao sofre os efeitos mas eh reconhecido
+    {
+        if (outro.gameObject.CompareTag("lapis"))
+        {
+            SimboNumero.instance.SalvaPontos(1);
+        }
+        else if (outro.gameObject.CompareTag("lapis5"))
+        {
+            SimboNumero.instance.SalvaPontos(5);
+        }
+        else if (outro.gameObject.CompareTag("lapis10"))
+        {
+            SimboNumero.instance.SalvaPontos(10);
+        }
+    }
+
+    private void OnTriggerExit2D(Collider2D outro)
+    {
+        if (outro.gameObject.CompareTag("lapis"))
+        {
+            SimboNumero.instance.TiraPontos(1);
+        }
+        else if (outro.gameObject.CompareTag("lapis5"))
+        {
+            SimboNumero.instance.TiraPontos(5);
+        }
+        else if (outro.gameObject.CompareTag("lapis10"))
+        {
+            SimboNumero.instance.TiraPontos(10);
+        }
+    }
+
+}
