008f9f4d73d563a4c70f26fb6de3b9664d340d69
Nícholas Lima de Souza Silva <nicholaslima.rw@gmail.com>
2019-09-19 08:35:42 -0300

    Revert "Languages"

---
+++ Assets/Principal/Scritps/Jogos/AVD/Vestir/Meia.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class Meia : MonoBehaviour {
+
+    private void OnTriggerEnter2D(Collider2D outro)
+    {
+        if (outro.name == "tenis" && gameObject.GetComponentInParent<Image>().sprite.name=="teo-meia")
+        {
+
+            VestirManager.instance.rightClothe = true;
+
+        }
+        else
+        {
+            VestirManager.instance.rightClothe = false;
+
+        }
+    }
+}
---
+++ Assets/Principal/Scritps/Menu/SliderMusica.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class SliderMusica : MonoBehaviour {
+
+    private Slider slider;
+
+	// Use this for initialization
+	void Start () {
+        slider = gameObject.GetComponent<Slider>();
+        if (PlayerPrefs.HasKey("music_volume"))
+        {
+            slider.value = PlayerPrefs.GetFloat("music_volume");
+        }
+        
+    }
+
+    public void OnSliderChanged()
+    {
+        Musica.instance.OnChangeMusicVolum(slider.value);
+        PlayerPrefs.SetFloat("music_volume", slider.value);
+    }
+}
